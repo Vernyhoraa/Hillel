109793b12886d95243df7f90eb5a7931186ebaa7
Vernyhoraa <vernyhora.elena@gmail.com>
2022-11-08 23:55:49 +0200

    homeWork 4

---
+++ src/main/java/lesson4/Util.java
@@ -0,0 +1,35 @@
+package lesson4;
+
+//Создать класс Util и 2 статических метода в нем которые принимать массив типа int
+public class Util {
+    //    вывести в консоль сумму всех чисел больше 5 и меньше 45 (для подсчета использовать цикл)
+    public static int MathUtils1(int[] mass) {
+        int result = 0;
+        for (int i = 0; i < mass.length; i++) {
+            if (mass[i] > 5 && mass[i] < 45) {
+                result = result + mass[i];
+            }
+        }
+        return result;
+    }
+
+    //    вывести в консоль все четные положительные числа.
+    public static void MathUtils2(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0 && array[i] > 0) {
+                System.out.println("четное положительное: " + array[i]);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/lesson4/lesson4.java
@@ -0,0 +1,12 @@
+package lesson4;
+
+
+import java.util.Arrays;
+
+public class lesson4 {
+    public static void main(String[] args) {
+     System.out.println(Util.MathUtils1(new int[]{1, 10, 18, 44, 2, 8, 5, 13, 70, 2, 6, 9}));
+     Util.MathUtils2(new int[]{1, -10, -18, 44, 2, 8, 5, 13, -70, 6, 9});
+    }
+}
+
