591da524dc20e622e46ac018d0ba37a856ee811c
Vernyhoraa <vernyhora.elena@gmail.com>
2022-11-09 10:18:21 +0200

    eddit

--- src/main/java/lesson4/Util.java
+++ src/main/java/lesson4/Util.java
@@ -3,25 +3,24 @@ package lesson4;
 //Создать класс Util и 2 статических метода в нем которые принимать массив типа int
 public class Util {
     //    вывести в консоль сумму всех чисел больше 5 и меньше 45 (для подсчета использовать цикл)
-    public static int sum(int[] mass) {
+    public static int mathUtils1(int[] mass) {
         int result = 0;
-        for (int j : mass) {
-            if (j > 5 && j < 45) {
-                result = result + j;
+        for (int i = 0; i < mass.length; i++) {
+            if (mass[i] > 5 && mass[i] < 45) {
+                result = result + mass[i];
             }
         }
         return result;
     }
 
     //    вывести в консоль все четные положительные числа.
-    public static void positivePrint(int[] array) {
-        for (int j : array) {
-            if (j % 2 == 0 && j > 0) {
-                System.out.println("четное положительное: " + j);
+    public static void mathUtils2(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0 && array[i] > 0) {
+                System.out.println("четное положительное: " + array[i]);
             }
         }
     }
-
 }
 
 
--- src/main/java/lesson4/lesson4.java
+++ src/main/java/lesson4/lesson4.java
@@ -3,14 +3,10 @@ package lesson4;
 
 import java.util.Arrays;
 
-import static lesson4.Util.sum;
-
 public class lesson4 {
     public static void main(String[] args) {
-        int[] arrays = {1, -10, -18, 44, 2, 8, 5, 13, -70, 6, 9};
-        int summary = sum(arrays);
-        System.out.println("SUM: " + summary);
-        Util.positivePrint(arrays);
+     System.out.println(Util.mathUtils1(new int[]{1, 10, 18, 44, 2, 8, 5, 13, 70, 2, 6, 9}));
+     Util.mathUtils2(new int[]{1, -10, 18, 44, 2, -8, 5, 13, 70, 2, 6, 9});
     }
 }
 
